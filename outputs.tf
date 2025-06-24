302c06b4f40e305fe0ab55fab6a213c846008eed
wilsonhm21 <whuacasimamani@gmail.com>
2025-06-24 01:33:34 -0500

    Despliegue automático con subida de repuestos.csv con timestamp

--- outputs.tf
+++ outputs.tf
@@ -7,3 +7,13 @@ output "ec2_ip_publica" {
   description = "Dirección IP pública de la instancia EC2"
   value       = module.ec2.ec2_public_ip
 }
+
+output "input_bucket" {
+  description = "Nombre real del bucket de entrada S3"
+  value       = module.s3.input_bucket
+}
+
+output "output_bucket" {
+  description = "Nombre real del bucket de salida S3"
+  value       = module.s3.output_bucket
+}
